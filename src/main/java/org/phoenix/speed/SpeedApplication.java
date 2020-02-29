6c9fced3c481ccc53b8578995e141e6ad474adf9
秦治辉 <>
2020-02-29 17:16:12 +0800

    初始化项目基本文件，可支持启动

---
+++ src/main/java/org/phoenix/speed/SpeedApplication.java
@@ -0,0 +1,32 @@
+package org.phoenix.speed;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class SpeedApplication {
+
+	private static Logger logger= LoggerFactory.getLogger(SpeedApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext application=SpringApplication.run(SpeedApplication.class, args);
+		Environment env = application.getEnvironment();
+		logger.error("\n----------------------------------------------------------\n\t" +
+						"Welcome to Speed! \n\t" +
+						"Access URLs:\n\t" +
+						"Local: \t\thttp://127.0.0.1:{}\n\t" +
+						"External: \thttp://{}:{}\n"+
+						"----------------------------------------------------------",
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
+	}
+
+}
---
+++ src/test/java/org/phoenix/speed/SpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.phoenix.speed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
